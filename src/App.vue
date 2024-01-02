930910d9eaa9c85fe99e45baf41807c6055521da
Lynerd <tilleryl10@gmail.com>
2024-01-01 20:45:09 -0500

    Al woring but title becoming phantom items

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <script setup>
 import { ref } from 'vue'
-import ListofLists from './components/ListofLists.vue'
-import createList from './components/createList.vue'
+import ListOfLists from './components/ListOfLists.vue'
 </script>
 
 <template>
   <div>
     <h1>Buy Good Things</h1>
-    <div class="make-list">
-      <createList />
-    </div>
-    <ListofLists />
+    <ListOfLists />
   </div>
 </template>
 
